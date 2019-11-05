c4394d055c03110c23ac2bd4a05d73a543390913
Eben Mallory <ebenryanmallory@gmail.com>
2019-11-05 15:59:10 -0600

    styling added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
